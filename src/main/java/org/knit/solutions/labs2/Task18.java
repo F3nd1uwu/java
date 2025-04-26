8dfc8b3563024a121810ed33e464c770b40c5543
fendi <fendi.xxd@gmail.com>
2025-04-26 11:33:20 +0400

    Tasks 18 начал работу Создал запускающий класс

---
+++ src/main/java/org/knit/solutions/labs2/Task18.java
@@ -0,0 +1,12 @@
+package org.knit.solutions.labs2;
+
+import org.knit.TaskDescription;
+import org.knit.solutions.Solution;
+
+@TaskDescription(taskNumber = 18, taskDescription = "Задача 18")
+public class Task18 implements Solution {
+    @Override
+    public void execute() {
+
+    }
+}
